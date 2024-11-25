2c7af6b25c91d483c2dd9534a9a5cb313b4f7a35
Aaron Gladston <aarongladston2003@gmail.com>
2024-11-25 15:41:00 +0530

    Nov 25

---
+++ 20.py
@@ -0,0 +1,3 @@
+colors=input("Enter the colours separated by a comma:\n").split(",")
+print("The first colour in the list is:",colors[0],"\nThe last colour in the list is:",colors[-1])
+
---
+++ 21.py
@@ -0,0 +1,21 @@
+dict={}
+key=[]
+values=[]
+n=int(input("Enter the number of enteries to be made in the dictionary:\n"))
+print("Input the keys of the dictionary")
+for i in range(n):
+    key.append(int(input()))
+print("Input the corresponding values of keys")
+for i in range(n):
+    values.append(input())
+dict={key[i]:values[i] for i in range(n)}
+print(dict)
+
+    
+
+
+
+
+
+
+

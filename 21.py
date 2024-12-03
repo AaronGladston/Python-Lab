c095a66c7da5b234a805e0b83d5846cf244e9316
AaronGladston <aarongladston2003@gmail.com>
2024-12-03 11:15:53 +0530

    Update 21.py

--- 21.py
+++ 21.py
@@ -1,4 +1,4 @@
-dict={}
+dict1={}
 key=[]
 values=[]
 n=int(input("Enter the number of enteries to be made in the dictionary:\n"))
@@ -8,19 +8,16 @@ for i in range(n):
 print("Input the values of the corresponding keys")
 for i in range(n):
     values.append(input())
-dict={key[i]:values[i] for i in range(n)}
-print("Initial dictionary:",dict)
-keys=list(dict.keys())
+dict1={key[i]:values[i] for i in range(n)}
+print("Initial dictionary:",dict1)
+keys=list(dict1.keys())
 keys.sort()
 print("The dictionary sorted in ascending order is:")
-for k in keys:
-    print(f"{k}: \'{dict[k]}\'",end=",")
-print("\n")
+ascending_dict = {k: dict1[k] for k in keys}
+print(ascending_dict)
 print("The dictionary sorted in descending order is:")
-keys.reverse()
-for k in keys:
-    print(f"{k}: \'{dict[k]}\'",end=",")
-
+descending_dict = {k: dict1[k] for k in reversed(keys)}
+print(descending_dict)
 
 
 

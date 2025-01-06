4708cfb04475ea93b88e5079c23bcc03d0a34856
Aaron Gladston <aarongladston2003@gmail.com>
2025-01-06 10:05:58 +0530

    Update 33_ColsCSV.py

--- 33_ColsCSV.py
+++ 33_ColsCSV.py
@@ -1,9 +1,8 @@
 import csv
-with open("33_file.csv","r") as csvfile:
+with open("readcsv.csv","r") as csvfile:
     reader = csv.reader(csvfile)
-    file = next(reader)
     n = int(input("Enter the specific columns of your choice: "))
     col = []
     for row in reader:
         col.append(row[n])
-    print(file[n],"\n",col)
+    print(col)
